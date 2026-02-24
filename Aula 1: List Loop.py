a9d7edac51a75380fdc93da456e96e1aaa5a675c
Gustavo Mendes Dourado Viana <gustavomendes@MacBook-Air-de-Gustavo.local>
2026-02-24 08:55:28 -0300

    aula 2

--- Aula 1: List Loop.py
+++ Aula 1: List Loop.py
@@ -1,3 +1,22 @@
+# Questão 21
+
+nomes = ["Ana", "Pedro", "Maria", "João"]
+
+# Questão 22
+
+for nome in nomes:
+    print(nome)
+
+# Questão 23
+
+nomes_maiusculos = []
+
+for nome in nomes:
+    nome_maisculo = nome.upper()
+    nomes_maiusculos.append(nome_maisculo)
+
+print(nomes_maiusculos)
+
 # Questão 24
 
 numeros = range(0,20)
@@ -14,4 +33,18 @@ print(numeros_pares)
 quadrados = []
 
 for numero in numeros:
-    quadrado = numero ** 2\
+    quadrado = numero ** 2
+    quadrados.append(quadrado)
+
+print(quadrados)
+
+# Questão 26
+
+palavras = ["python", "java", "c", "javascript"]
+caracteres = []
+
+for palavra in palavras:
+    caractere = len(palavra)
+    caracteres.append(caractere)
+
+print(caracteres)
---
+++ Aula 2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_csv('/Users/gustavomendes/Faculdade/3° Semestre/Programação para Análise de Dados/notas.csv')
+
+df.shape
+df.columns
+df.dtypes
+df.isna().sum()
+df.head()
+df.tail()

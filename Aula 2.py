82c13614781471f9b8ebe50243f48bb09507c0d9
Gustavo Mendes Dourado Viana <gustavomendes@MacBook-Air-de-Gustavo.local>
2026-03-05 09:09:01 -0300

    nova funcao

--- Aula 2.py
+++ Aula 2.py
@@ -3,7 +3,11 @@ import pandas as pd
 df = pd.read_csv('/Users/gustavomendes/Faculdade/3° Semestre/Programação para Análise de Dados/notas.csv')
 
 df.shape
+df.dtypes
+df.isnull().sum()
 df.columns
+df.loc[:, "year"].unique()
+df.loc[:, "country"].unique()
 df.dtypes
 df.isna().sum()
 df.head()
